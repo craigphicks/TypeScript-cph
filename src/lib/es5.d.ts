6029a01f5f727a4326b7cdda8a9d4327d2c66a5b
craigphicks <13205732+craigphicks@users.noreply.github.com>
2023-10-15 12:30:18 +0000

    es5.d.ts + BooleanConverter for filter.  Boolean Constructor would be wrong!

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -536,6 +536,11 @@ interface BooleanConstructor {
     readonly prototype: Boolean;
 }
 
+interface BooleanConverter {
+    <T>(value?: T): boolean;
+    readonly prototype: Boolean;
+}
+
 declare var Boolean: BooleanConstructor;
 
 interface Number {
@@ -1257,7 +1262,7 @@ interface ReadonlyArray<T> {
      * @param thisArg An object to which the this keyword can refer in the predicate function. If thisArg is omitted, undefined is used as the this value.
      */
     filter<S extends T>(predicate: (value: T, index: number, array: readonly T[]) => value is S, thisArg?: any): S[];
-    filter(predicate: BooleanConstructor, thisArg?: any): (T extends false | 0 | "" | null | undefined | 0n ? never : T)[];
+    filter(predicate: BooleanConverter, thisArg?: any): (T extends false | 0 | "" | null | undefined | 0n ? never : T)[];
     /**
      * Returns the elements of an array that meet the condition specified in a callback function.
      * @param predicate A function that accepts up to three arguments. The filter method calls the predicate function one time for each element in the array.
@@ -1449,7 +1454,7 @@ interface Array<T> {
      * @param thisArg An object to which the this keyword can refer in the predicate function. If thisArg is omitted, undefined is used as the this value.
      */
     filter<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): S[];
-    filter(predicate: BooleanConstructor, thisArg?: any): (T extends false | 0 | "" | null | undefined | 0n ? never : T)[];
+    filter(predicate: BooleanConverter, thisArg?: any): (T extends false | 0 | "" | null | undefined | 0n ? never : T)[];
     /**
      * Returns the elements of an array that meet the condition specified in a callback function.
      * @param predicate A function that accepts up to three arguments. The filter method calls the predicate function one time for each element in the array.
