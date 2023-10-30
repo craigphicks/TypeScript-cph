0118098b6cc39ee51971667621a2aea3e7a04a45
craigphicks <13205732+craigphicks@users.noreply.github.com>
2023-10-30 19:11:46 +0000

    es5.d.ts with added overload (Read)Array.filter(predicate: BooleanConverter, ...): NonFalsy<T>

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -536,6 +536,11 @@ interface BooleanConstructor {
     readonly prototype: Boolean;
 }
 
+interface BooleanConverter {
+    <T>(value?: T): boolean;
+    readonly prototype: Boolean;
+}
+
 declare var Boolean: BooleanConstructor;
 
 interface Number {
@@ -1258,6 +1263,12 @@ interface ReadonlyArray<T> {
      */
     filter<S extends T>(predicate: (value: T, index: number, array: readonly T[]) => value is S, thisArg?: any): S[];
     /**
+     * Returns the non-Falsy elements of an array
+     * @param predicate Must be exactly "Boolean"
+     * @param thisArg An object to which the this keyword can refer in the predicate function. If thisArg is omitted, undefined is used as the this value.
+     */
+    filter(predicate: BooleanConverter, thisArg?: any): (T extends false | 0 | "" | null | undefined | 0n ? never : T)[];
+    /**
      * Returns the elements of an array that meet the condition specified in a callback function.
      * @param predicate A function that accepts up to three arguments. The filter method calls the predicate function one time for each element in the array.
      * @param thisArg An object to which the this keyword can refer in the predicate function. If thisArg is omitted, undefined is used as the this value.
@@ -1448,6 +1459,12 @@ interface Array<T> {
      * @param thisArg An object to which the this keyword can refer in the predicate function. If thisArg is omitted, undefined is used as the this value.
      */
     filter<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): S[];
+    /**
+     * Returns the non-Falsy elements of an array
+     * @param predicate Must be exactly "Boolean"
+     * @param thisArg An object to which the this keyword can refer in the predicate function. If thisArg is omitted, undefined is used as the this value.
+     */
+    filter(predicate: BooleanConverter, thisArg?: any): (T extends false | 0 | "" | null | undefined | 0n ? never : T)[];
     /**
      * Returns the elements of an array that meet the condition specified in a callback function.
      * @param predicate A function that accepts up to three arguments. The filter method calls the predicate function one time for each element in the array.
