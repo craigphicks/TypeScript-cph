25e36e78bfaf9c833c870c92b680baf15cf82215
craigphicks <13205732+craigphicks@users.noreply.github.com>
2023-10-31 19:56:23 +0000

    new tests case booleanConverter.ts, baseline-accepted

--- .bash_history
+++ .bash_history
@@ -2,3 +2,13 @@ git checkout -f 61a96b1641abe24c4adc3633eb936df89eb991f2
 git switch -c origin-main-nolibchange
 hereby runtests-parallel 
 git status
+git add .
+git commit -m "code change only"
+hereby runtests --test="booleanConverter.ts"
+hereby baseline-accept
+git status
+git commit -m "new tests case booleanConverter.ts, baseline-accepted" 
+git log
+git commit -m "new tests case booleanConverter.ts, baseline-accepted" 
+git commit -f -m "new tests case booleanConverter.ts, baseline-accepted" 
+git commit -m "new tests case booleanConverter.ts, baseline-accepted" 
---
+++ tests/baselines/reference/booleanConverter.js
@@ -0,0 +1,48 @@
+//// [tests/cases/compiler/booleanConverter.ts] ////
+
+//// [booleanConverter.ts]
+const nullableValues = ['a', 'b', null];
+
+const values1 = nullableValues.filter(Boolean);
+
+// @ts-expect-error
+const values2 = nullableValues.filter(new Boolean);
+
+const arr = [0, 1, "", "foo", null] as const;
+
+const arr2 = arr.filter(Boolean);
+
+declare const t:undefined | {};
+
+const x = Boolean(t) ? true : false;
+
+x;
+
+if (Boolean(t)){
+    t;
+}
+
+
+//// [booleanConverter.js]
+"use strict";
+const nullableValues = ['a', 'b', null];
+const values1 = nullableValues.filter(Boolean);
+// @ts-expect-error
+const values2 = nullableValues.filter(new Boolean);
+const arr = [0, 1, "", "foo", null];
+const arr2 = arr.filter(Boolean);
+const x = Boolean(t) ? true : false;
+x;
+if (Boolean(t)) {
+    t;
+}
+
+
+//// [booleanConverter.d.ts]
+declare const nullableValues: (string | null)[];
+declare const values1: (string | null)[];
+declare const values2: (string | null)[];
+declare const arr: readonly [0, 1, "", "foo", null];
+declare const arr2: ("" | 0 | 1 | "foo" | null)[];
+declare const t: undefined | {};
+declare const x: boolean;
