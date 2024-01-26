70b9390d844335f8837b68fa36d020220b548997
craigphicks <13205732+craigphicks@users.noreply.github.com>
2024-01-26 18:54:34 +0000

    57087 backup -57087-2(1|2).ts tests and results (test fail mode)

---
+++ tests/baselines/reference/-57087-21.js
@@ -0,0 +1,25 @@
+//// [tests/cases/compiler/-test/-57087-21.ts] ////
+
+//// [-57087-21.ts]
+// test f domain does not support Garg domain (3 omitted from f domain) - should not satisfy
+
+declare const f1: { (x: 1 | 2): 1 | 2; (x: 2): "2" | "3"; (x: 1 | 2): 1 | 2 | "2" |"3";}
+
+type Garg1 = ((x: 1 | 2) => 1 | 2) & ((x: 2 | 3) => "2" | "3");
+
+f1 satisfies Garg1; // should not satisfy
+
+
+//// [-57087-21.js]
+"use strict";
+// test f domain does not support Garg domain (3 omitted from f domain) - should not satisfy
+f1; // should not satisfy
+
+
+//// [-57087-21.d.ts]
+declare const f1: {
+    (x: 1 | 2): 1 | 2;
+    (x: 2): "2" | "3";
+    (x: 1 | 2): 1 | 2 | "2" | "3";
+};
+type Garg1 = ((x: 1 | 2) => 1 | 2) & ((x: 2 | 3) => "2" | "3");
