36e5502aebfede03659f528593c894f870e83c10
craigphicks <13205732+craigphicks@users.noreply.github.com>
2024-02-09 20:56:19 +0000

    57087 backup tests

---
+++ tests/cases/compiler/-test/-57087-unionOfClassCalls-32.ts
@@ -0,0 +1,22 @@
+// @strict: true
+
+interface MyArr<T> {
+    /**
+     * Calls the specified callback function for all the elements in an array. The return value of the callback function is the accumulated result, and is provided as an argument in the next call to the callback function.
+     * @param callbackfn A function that accepts up to four arguments. The reduce method calls the callbackfn function one time for each element in the array.
+     * @param initialValue If initialValue is specified, it is used as the initial value to start the accumulation. The first call to the callbackfn function provides this value as an argument instead of an array value.
+     */
+    reduce(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T): T;
+    reduce(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T, initialValue: T): T;
+    /**
+     * Calls the specified callback function for all the elements in an array. The return value of the callback function is the accumulated result, and is provided as an argument in the next call to the callback function.
+     * @param callbackfn A function that accepts up to four arguments. The reduce method calls the callbackfn function one time for each element in the array.
+     * @param initialValue If initialValue is specified, it is used as the initial value to start the accumulation. The first call to the callbackfn function provides this value as an argument instead of an array value.
+     */
+    reduce<U>(callbackfn: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U, initialValue: U): U;
+}
+declare const arr: MyArr<number> | MyArr<string>;  // Works with Array<number | string>
+
+{
+    const t = arr.reduce((acc,a) => acc+a)
+}
